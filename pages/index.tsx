52d4ae935c713632cadfa82e8e6d3d367e293978
Vienio99 <kyve96@gmail.com>
2022-02-26 21:05:21 +0100

    initial setup with emotion css for styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+const titleStyle = css({
+  background: 'pink'
+})
+
 
 const Home: NextPage = () => {
   return (
@@ -13,11 +20,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 css={titleStyle}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p css={css`background-color: pink`}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
@@ -52,19 +59,6 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
